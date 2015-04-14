71066885762d9d84a3b9dce3220256168fdb19a1
Alastair Daivis <alastair@outware.com.au>
2015-04-14 17:27:09 +1000

    Get environment variables from a login shell
    ...before forking nvim. Make sure a login shell is used, use SHELL
    environment variable if set (otherwise fall back to BASH)

--- src/app.mm
+++ src/app.mm
@@ -67,6 +67,73 @@ static NSWindow *window = 0;
     return YES;
 }
 
+- (NSDictionary *)environmentFromLoginShell:(NSString *)shellPath
+{
+    if (!shellPath.length) {
+        return nil;
+    }
+
+    NSArray *args = @[@"-l", @"-c", @"\"env\""];
+    NSTask *task = [NSTask new];
+    task.launchPath = shellPath;
+    task.arguments = args;
+    task.standardOutput = [NSPipe new];
+    task.standardError = [NSPipe new];
+    [task launch];
+    [task waitUntilExit];
+
+    if (task.terminationStatus != EXIT_SUCCESS) {
+        NSData *stderrData = ((NSPipe *)task.standardError).fileHandleForReading.readDataToEndOfFile;
+        NSString *stderror = [[NSString alloc] initWithData:stderrData encoding:NSUTF8StringEncoding];
+        NSLog(@"%@ %@ failed: %@", task.launchPath, [task.arguments componentsJoinedByString:@" "], stderror);
+        return nil;
+    }
+
+    NSData *stdoutData = ((NSPipe *)task.standardOutput).fileHandleForReading.readDataToEndOfFile;
+    NSString *envString = [[NSString alloc] initWithData:stdoutData encoding:NSUTF8StringEncoding];
+    NSArray *envVars = [envString componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSMutableDictionary *env = [NSMutableDictionary new];
+
+    for (NSString *s in envVars) {
+        NSArray *keyvalue = [s componentsSeparatedByString:@"="];
+
+        if (keyvalue.count < 2) {
+            continue;
+        }
+
+        NSString *key = keyvalue[0];
+        NSString *value = keyvalue[1];
+
+        env[key] = value;
+    }
+
+    return env;
+}
+
+/**  
+ *  Attempt to get the environment dictionary for the user's chosen shell,
+ *  using the $SHELL environment variable.
+ *  
+ *  If that fails, try again using /bin/bash, which should always be available on OSX.
+ */
+- (void)loadLoginShellEnvironmentVariables
+{
+    NSString *shellPath = [[NSProcessInfo processInfo] environment][@"SHELL"];
+    NSDictionary *env = [self environmentFromLoginShell:shellPath];
+    if (!env) {
+        shellPath = @"/bin/bash";
+        env = [self environmentFromLoginShell:shellPath];
+    }
+
+    if (!env) {
+        NSLog(@"Couldn't get environment from $SHELL or /bin/bash, defaulting to existing environment.");
+    } else {
+        [env enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *value, BOOL *stop) {
+            setenv(key.UTF8String, value.UTF8String, 1);
+        }];
+    }
+}
+
 - (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
     [NSFontManager setFontManagerFactory:[VimFontManager class]];
@@ -74,9 +141,9 @@ static NSWindow *window = 0;
 
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults registerDefaults:@{@"width": @80,
-                                @"height": @25,
-                              @"fontName": @"Menlo",
-                              @"fontSize": @11.0}];
+                                 @"height": @25,
+                                 @"fontName": @"Menlo",
+                                 @"fontSize": @11.0}];
 
     int width = [defaults integerForKey:@"width"];
     int height = [defaults integerForKey:@"height"];
@@ -89,6 +156,8 @@ static NSWindow *window = 0;
     NSString *vimDir = [[NSBundle mainBundle] resourcePath];
     NSString *vimPath = [[NSBundle mainBundle] pathForResource:@"nvim"
                                                         ofType:nil];
+
+    [self loadLoginShellEnvironmentVariables];
 
     /* Set both VIM and NVIM for now. TODO: Remove VIM when
        https://github.com/neovim/neovim/pull/1927 is merged */
