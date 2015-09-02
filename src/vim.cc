241d054e20ca324d2497d7534ffe0d8b2f1d1dac
bambu <bambu@revengsec.com>
2015-09-01 22:39:38 -0400

    Determines whether we were started by launched or not

--- src/vim.cc
+++ src/vim.cc
@@ -13,9 +13,12 @@ const char *vim_argv[] = {"nvim", "--embed", 0};
 
 const char **vim_create_argv()
 {
+    if (!(isatty (STDIN_FILENO) || isatty (STDOUT_FILENO) || isatty(STDERR_FILENO)))
+        return vim_argv;
+
     if (g_argc == 1)
         return vim_argv;
-    
+
     const char **argv = new const char *[g_argc+2];
     argv[0] = vim_argv[0];
     argv[1] = vim_argv[1];
